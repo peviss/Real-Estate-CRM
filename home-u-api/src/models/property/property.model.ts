d20ef3de57d0ed031608b9322b3202942484058b
peviss <pvissillac@gmail.com>
2021-03-25 01:34:39 -0300

    feat: add title to property model

--- home-u-api/src/models/property/property.model.ts
+++ home-u-api/src/models/property/property.model.ts
@@ -3,7 +3,7 @@ import { Document } from 'mongoose'
 export default interface IProperty extends Document {
     //id: string;
     ref: string;
-    title: string:
+    title: string;
     description: string;
     location: Location;
     operation: Operation;
